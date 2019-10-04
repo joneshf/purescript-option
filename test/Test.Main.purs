ec0513aa6ae443524b83fb40befdc42c1b8c98d3
Brandon Elam Barker <beb82@cornell.edu>
2019-10-04 19:50:33 +0000

    added failing test for set

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,7 +1,19 @@
 module Test.Main (main) where
 
 import Prelude
+import Data.Maybe (Maybe(..))
+import Data.Symbol (SProxy(..))
 import Effect as Effect
+import Option as Opt
+import Test.Assert (assert)
+
+someOption :: Opt.Option ( foo :: Boolean, bar :: Int )
+someOption = Opt.empty
+bar1 :: Int
+bar1 = 31
+anotherOption :: Opt.Option ( foo :: Boolean, bar :: Int )
+anotherOption = Opt.set (SProxy :: _ "bar") bar1 someOption
 
 main :: Effect.Effect Unit
-main = pure unit
+main = do
+  assert $ Just bar1 == (Opt.get (SProxy :: _ "bar") anotherOption)
