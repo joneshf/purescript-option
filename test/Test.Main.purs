e0a98051fe2032a9afc894042636a4f0e9c849b3
joneshf <jones3.hardy@gmail.com>
2020-03-01 11:30:12 -0800

    Update test suite to use `purescript-spec`
    `purescript-assert` is really only useful for one-off low-level
    assertions. It doesn't provide any contextual information about what
    failed or why.
    
    Since we're writing a package that we expect to maintain for a while, we
    want to use a testing package that provides a situation that's a bit
    easier to use.

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,21 +1,35 @@
 module Test.Main (main) where
 
 import Prelude
-import Data.Maybe (Maybe(..))
-import Data.Symbol (SProxy(..))
+import Data.Maybe as Data.Maybe
 import Effect as Effect
-import Option as Opt
-import Test.Assert (assert)
+import Effect.Aff as Effect.Aff
+import Option as Option
+import Test.Spec as Test.Spec
+import Test.Spec.Assertions as Test.Spec.Assert
+import Test.Spec.Reporter.Console as Test.Spec.Reporter.Console
+import Test.Spec.Runner as Test.Spec.Runner
 
-someOption :: Opt.Option ( foo :: Boolean, bar :: Int )
-someOption = Opt.empty
-
-bar1 :: Int
-bar1 = 31
-
-anotherOption :: Opt.Option ( foo :: Boolean, bar :: Int )
-anotherOption = Opt.set (SProxy :: _ "bar") bar1 someOption
+data Proxy (symbol :: Symbol)
+  = Proxy
 
 main :: Effect.Effect Unit
-main = do
-  assert $ Just bar1 == (Opt.get (SProxy :: _ "bar") anotherOption)
+main = Effect.Aff.launchAff_ (Test.Spec.Runner.runSpec reporters spec)
+
+reporters :: Array Test.Spec.Runner.Reporter
+reporters =
+  [ Test.Spec.Reporter.Console.consoleReporter
+  ]
+
+spec :: Test.Spec.Spec Unit
+spec = do
+  Test.Spec.describe "Option" do
+    Test.Spec.describe "set" do
+      Test.Spec.it "sets a value when it doesn't exist" do
+        let
+          someOption :: Option.Option ( foo :: Boolean, bar :: Int )
+          someOption = Option.empty
+
+          anotherOption :: Option.Option ( foo :: Boolean, bar :: Int )
+          anotherOption = Option.set (Proxy :: _ "bar") 31 someOption
+        Option.get (Proxy :: _ "bar") anotherOption `Test.Spec.Assert.shouldEqual` Data.Maybe.Just 31
