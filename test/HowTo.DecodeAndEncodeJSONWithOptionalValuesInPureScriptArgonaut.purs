377a11e7003b61cd05e4cb328e1a9f2455b8f000
joneshf <jones3.hardy@gmail.com>
2020-10-04 19:36:17 -0700

    Bump `purescript-argonaut-codecs` to 7.0.0
    This version had a bunch of breaking changes we had to deal with.
    
    The primary thing that changed is the type of decoding. Since we
    implement some stuff directly depending on those types, we also
    introduce breaking changes.
    
    We also deal with the tests in a way that should mitigate how much we
    have to deal with in the future.

--- test/HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptArgonaut.purs
+++ test/HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptArgonaut.purs
@@ -7,7 +7,9 @@ module HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptArgonaut
 import Prelude
 import Data.Argonaut.Core as Data.Argonaut.Core
 import Data.Argonaut.Decode.Class as Data.Argonaut.Decode.Class
+import Data.Argonaut.Decode.Error as Data.Argonaut.Decode.Error
 import Data.Argonaut.Encode.Class as Data.Argonaut.Encode.Class
+import Data.Argonaut.Parser as Data.Argonaut.Parser
 import Data.Either as Data.Either
 import Option as Option
 import Test.Spec as Test.Spec
@@ -15,7 +17,7 @@ import Test.Spec.Assertions as Test.Spec.Assertions
 
 decode ::
   Data.Argonaut.Core.Json ->
-  Data.Either.Either String (Option.Option ( name :: String, title :: String ))
+  Data.Either.Either Data.Argonaut.Decode.Error.JsonDecodeError (Option.Option ( name :: String, title :: String ))
 decode = Data.Argonaut.Decode.Class.decodeJson
 
 encode ::
@@ -23,31 +25,20 @@ encode ::
   Data.Argonaut.Core.Json
 encode = Data.Argonaut.Encode.Class.encodeJson
 
+parse ::
+  String ->
+  Data.Either.Either String (Option.Option ( name :: String, title :: String ))
+parse string = case Data.Argonaut.Parser.jsonParser string of
+  Data.Either.Left error -> Data.Either.Left error
+  Data.Either.Right json -> case decode json of
+    Data.Either.Left error -> Data.Either.Left (Data.Argonaut.Decode.Error.printJsonDecodeError error)
+    Data.Either.Right option -> Data.Either.Right option
+
 spec :: Test.Spec.Spec Unit
 spec =
   Test.Spec.describe "HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptArgonaut" do
-    spec_decode
     spec_encode
-
-spec_decode :: Test.Spec.Spec Unit
-spec_decode =
-  Test.Spec.describe "decode" do
-    Test.Spec.it "doesn't require any fields" do
-      Test.Spec.Assertions.shouldEqual
-        (decode (Data.Argonaut.Encode.Class.encodeJson {}))
-        (Data.Either.Right (Option.fromRecord {}))
-    Test.Spec.it "accepts only a name" do
-      Test.Spec.Assertions.shouldEqual
-        (decode (Data.Argonaut.Encode.Class.encodeJson { name: "Pat" }))
-        (Data.Either.Right (Option.fromRecord { name: "Pat" }))
-    Test.Spec.it "accepts only a title" do
-      Test.Spec.Assertions.shouldEqual
-        (decode (Data.Argonaut.Encode.Class.encodeJson { title: "wonderful" }))
-        (Data.Either.Right (Option.fromRecord { title: "wonderful" }))
-    Test.Spec.it "accepts both a name and a title" do
-      Test.Spec.Assertions.shouldEqual
-        (decode (Data.Argonaut.Encode.Class.encodeJson { name: "Pat", title: "Dr." }))
-        (Data.Either.Right (Option.fromRecord { name: "Pat", title: "Dr." }))
+    spec_parse
 
 spec_encode :: Test.Spec.Spec Unit
 spec_encode =
@@ -68,3 +59,23 @@ spec_encode =
       Test.Spec.Assertions.shouldEqual
         (Data.Argonaut.Core.stringify (encode (Option.fromRecord { name: "Pat", title: "Dr." })))
         "{\"title\":\"Dr.\",\"name\":\"Pat\"}"
+
+spec_parse :: Test.Spec.Spec Unit
+spec_parse =
+  Test.Spec.describe "parse" do
+    Test.Spec.it "doesn't require any fields" do
+      Test.Spec.Assertions.shouldEqual
+        (parse """{}""")
+        (Data.Either.Right (Option.fromRecord {}))
+    Test.Spec.it "accepts only a name" do
+      Test.Spec.Assertions.shouldEqual
+        (parse """{ "name": "Pat" }""")
+        (Data.Either.Right (Option.fromRecord { name: "Pat" }))
+    Test.Spec.it "accepts only a title" do
+      Test.Spec.Assertions.shouldEqual
+        (parse """{ "title": "wonderful" }""")
+        (Data.Either.Right (Option.fromRecord { title: "wonderful" }))
+    Test.Spec.it "accepts both a name and a title" do
+      Test.Spec.Assertions.shouldEqual
+        (parse """{ "name": "Pat", "title": "Dr." }""")
+        (Data.Either.Right (Option.fromRecord { name: "Pat", title: "Dr." }))
