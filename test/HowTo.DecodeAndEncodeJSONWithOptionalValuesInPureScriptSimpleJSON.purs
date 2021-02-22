11e3f2bd67565c54f841cf2f10e6a687d8d4721a
joneshf <jones3.hardy@gmail.com>
2021-02-22 08:37:28 -0800

    Inline values
    We don't need to let-bind these values.
    
    The README doesn't let-bind a bunch of stuff. And if it's good enough
    for external documentation, it ought to be good enough for internal
    tests.

--- test/HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptSimpleJSON.purs
+++ test/HowTo.DecodeAndEncodeJSONWithOptionalValuesInPureScriptSimpleJSON.purs
@@ -31,83 +31,50 @@ spec_readJSON :: Test.Spec.Spec Unit
 spec_readJSON =
   Test.Spec.describe "readJSON" do
     Test.Spec.it "doesn't require any fields" do
-      let
-        json :: String
-        json = """{}"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{}""")
         (Data.Either.Right (Option.fromRecord {}))
     Test.Spec.it "accepts only a name" do
-      let
-        json :: String
-        json = """{ "name": "Pat" }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "name": "Pat" }""")
         (Data.Either.Right (Option.fromRecord { name: "Pat" }))
     Test.Spec.it "accepts only a title" do
-      let
-        json :: String
-        json = """{ "title": "wonderful" }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "title": "wonderful" }""")
         (Data.Either.Right (Option.fromRecord { title: "wonderful" }))
     Test.Spec.it "accepts both a name and a title" do
-      let
-        json :: String
-        json = """{ "name": "Pat", "title": "Dr." }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "name": "Pat", "title": "Dr." }""")
         (Data.Either.Right (Option.fromRecord { name: "Pat", title: "Dr." }))
     Test.Spec.it "doesn't fail a null name" do
-      let
-        json :: String
-        json = """{ "name": null }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "name": null }""")
         (Data.Either.Right (Option.fromRecord {}))
     Test.Spec.it "doesn't fail for a null title" do
-      let
-        json :: String
-        json = """{ "title": null }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "title": null }""")
         (Data.Either.Right (Option.fromRecord {}))
     Test.Spec.it "doesn't fail for a null name or title" do
-      let
-        json :: String
-        json = """{ "name": null, "title": null }"""
       Test.Spec.Assertions.shouldEqual
-        (readJSON json)
+        (readJSON """{ "name": null, "title": null }""")
         (Data.Either.Right (Option.fromRecord {}))
 
 spec_writeJSON :: Test.Spec.Spec Unit
 spec_writeJSON =
   Test.Spec.describe "writeJSON" do
     Test.Spec.it "inserts no fields if none exist" do
-      let
-        option :: Option.Option ( name :: String, title :: String )
-        option = Option.fromRecord {}
       Test.Spec.Assertions.shouldEqual
-        (writeJSON option)
+        (writeJSON (Option.fromRecord {}))
         "{}"
     Test.Spec.it "inserts a name if it exist" do
-      let
-        option :: Option.Option ( name :: String, title :: String )
-        option = Option.fromRecord { name: "Pat" }
       Test.Spec.Assertions.shouldEqual
-        (writeJSON option)
+        (writeJSON (Option.fromRecord { name: "Pat" }))
         "{\"name\":\"Pat\"}"
     Test.Spec.it "inserts a title if it exist" do
-      let
-        option :: Option.Option ( name :: String, title :: String )
-        option = Option.fromRecord { title: "wonderful" }
       Test.Spec.Assertions.shouldEqual
-        (writeJSON option)
+        (writeJSON (Option.fromRecord { title: "wonderful" }))
         "{\"title\":\"wonderful\"}"
     Test.Spec.it "inserts both a name and a title if they exist" do
-      let
-        option :: Option.Option ( name :: String, title :: String )
-        option = Option.fromRecord { name: "Pat", title: "Dr." }
       Test.Spec.Assertions.shouldEqual
-        (writeJSON option)
+        (writeJSON (Option.fromRecord { name: "Pat", title: "Dr." }))
         "{\"title\":\"Dr.\",\"name\":\"Pat\"}"
