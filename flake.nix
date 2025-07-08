7f76158730a98bc99a81c53b449351dbc131c639
joneshf <jones3.hardy@gmail.com>
2025-07-07 21:08:00 -0700

    Support ARM platforms in `nix`
    Times have moved on, and devices are using ARM more and more these days.
    In order to support that in this repository with the version of things
    we're using, we have to do a bit of work.
    
    The version of `purescript` we were using–0.14.0–does not have a
    pre-built ARM version. The `npm` package for `purescript` will fallback
    to building from source, but we have to make sure certain dependencies
    are available in order to do that. Since those dependencies are only
    necessary if we have to build from source, we make them conditional on
    the host platform of the system.
    
    Unfortunately even with those dependencies, we still cannot build things
    from source. The version of GHC that `purescript` 0.14.0 wants is
    8.10.4, which is not available in the version of `stack`? we're using.
    We could go down to an older version of `nixpkgs`, but the alternative
    we decide on is to update the version of `purescript` to 0.14.9. This
    version of `purescript` requires a newer version of GHC that actually is
    available in the `stack`? we're using.
    
    All this stuff means we can now develop again in the year 2025!

--- flake.nix
+++ flake.nix
@@ -37,17 +37,30 @@
         {
           devShells = {
             default = pkgs.mkShellNoCC {
-              nativeBuildInputs = [
-                pkgs.coreutils
-                pkgs.findutils
-                pkgs.gnumake
-                pkgs.nodejs_22
-              ];
+              nativeBuildInputs =
+                [
+                  pkgs.coreutils
+                  pkgs.findutils
+                  pkgs.gnumake
+                  pkgs.nodejs_22
+                ]
+                # The version of PureScript we use doesn't have pre-built binaries for ARM architectures.
+                # The `npm` package will fall back to building from source,
+                # but we need somre more dependencies in order to do that.
+                # We should be able to remove this once we update to at least version [0.15.9](https://github.com/purescript/purescript/releases/tag/v0.15.9) of PureScript,
+                # as this was the first version to start producing pre-built ARM binaries: https://github.com/purescript/purescript/pull/4455.
+                ++ pkgs.lib.lists.optionals pkgs.stdenv.hostPlatform.isAarch [
+                  pkgs.llvmPackages_12.libcxxClang
+                  pkgs.llvmPackages_12.libllvm
+                  pkgs.stack
+                ];
             };
           };
         };
 
       systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
